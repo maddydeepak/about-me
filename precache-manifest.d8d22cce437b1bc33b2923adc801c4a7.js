7a706da8639cc0a8307cbd059e9b5c644abdb2ed
maddydeepak <maddheshiya.deepak@gmail.com>
2020-05-13 12:24:03 +0530

    Updates

---
+++ precache-manifest.d8d22cce437b1bc33b2923adc801c4a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c7d202b04dedbb2d5116be8bfba97e",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "86c6285f692b38eb48ee",
+    "url": "/about-me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d73415663b5f5f63ee2",
+    "url": "/about-me/static/js/2.a12f7d7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about-me/static/js/2.a12f7d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c6285f692b38eb48ee",
+    "url": "/about-me/static/js/main.0ca435d4.chunk.js"
+  },
+  {
+    "revision": "8dbe29bd45939b759b63",
+    "url": "/about-me/static/js/runtime-main.9e30c24b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about-me/static/media/logo.5d5d9eef.svg"
+  }
+]);
